5089b89dfe1021397b2a7bd596662b02dc527151
Altaf Hussain <hussainmdaltaf8@gmail.com>
2021-09-13 21:20:41 +0530

    make it better

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV==="production")
 {
     app.use(express.static(path.resolve(__dirname, "./frontend/build")));
 
-    app.get("*", function (request, response) {
+    app.get("*",  (request, response)=> {
         response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
       });
 }
