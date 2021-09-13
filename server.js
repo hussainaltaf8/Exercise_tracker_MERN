01e1da75151eb7601777c955b97fb00fa51bf9db
Altaf Hussain <hussainmdaltaf8@gmail.com>
2021-09-13 21:09:50 +0530

    path resolve

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ app.use('/users', usersRouter);
 
 if(process.env.NODE_ENV==="production")
 {
-    app.use(express.static( "frontend/build"));
+    app.use(express.static(path.resolve(__dirname, "./frontend/build")));
 
-    app.get('*',(req, res) => {
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
-    });
+    app.get("*", function (request, response) {
+        response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+      });
 }
 
 app.listen(port, () => {
